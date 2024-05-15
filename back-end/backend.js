e3cade9879be5b1a041ec02b2ea5098b5ec27065
TOONZK <126704941+LeighVirrey@users.noreply.github.com>
2024-05-15 15:17:47 -0600

    facemesh model

--- back-end/backend.js
+++ back-end/backend.js
@@ -1,23 +1,29 @@
-import * as tf from '@tensorflow/tfjs-node';
-const facemesh = require('@tensorflow-models/facemesh');
-require('@tensorflow/tfjs-backend-wasm');
-const express = require('express');
-const cors = require('cors');
+import * as tf from '@tensorflow/tfjs';
+import * as facemesh from '@tensorflow-models/facemesh';
+import fs from 'fs'; //fs is just to test that the model is working and can actually read images
+import { Image, createCanvas } from 'canvas';
+import express from 'express';
+import cors from 'cors';
 const app = express();
-app.use(cors(origin = '*'));
+app.use(cors({origin: '*'}));
 const port = 9696;
+let net = null;
 
-async function loadFacemesh() {
-    const model = await facemesh.load();
-    return model;
-}
+app.get('/', async (req, res) => {
+    const img = new Image();
+    img.src = fs.readFileSync('paulfox.jpg');
+    const canvas = createCanvas(img.width, img.height);
+    const ctx = canvas.getContext('2d');
+    ctx.drawImage(img, 0, 0, img.width, img.height);
+    const face = await net.estimateFaces(canvas);
+    res.send(face);
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+    //This stuff is just for testing, we'll be improving the different end points of the back-end later, I just spent a while doing this.
 })
 
 app.listen(port, async () => {
     console.log(`Server is running on port ${port}`);
-    console.log('Facemesh model loaded');
+    net = await facemesh.load();
+    console.log('Model loaded');
 })
 
