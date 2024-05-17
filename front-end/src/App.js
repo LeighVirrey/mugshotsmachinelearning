a008e60391874a20ef91aeeaee22b3243c195b3a
kkingston32 <126709063+kkingston32@users.noreply.github.com>
2024-05-16 18:35:02 -0600

    upload image

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,46 @@
 import './App.css';
+import axios from "axios";
+import { useState } from 'react';
 
 function App() {
+  const [state, setState] = useState({selectedFile: null});
+  const [image, setImage] = useState("")
+
+  const fileSelectedHandler = event => {
+    setState({selectedFile: event.target.files[0]})
+    // setImage(event.target.files[0])
+
+    var fr = new FileReader();
+    fr.onload = function(){
+      setImage(fr.result);
+    }
+    fr.readAsDataURL(event.target.files[0]);
+
+    console.log("Event: ", event.target.files[0])
+  }
+  const fileUploadHandler = () => {
+    const fd = new FormData();
+    console.log(image)
+
+    // Push the image to your back-end
+
+    fd.append('image', state.selectedFile, state.selectedFile.name)
+    let url = "url to my back end";
+
+    axios.post('url',fd, {
+      onUploadProgress: ProgressEvent => { 
+        console.log("Upload Progress: " + Math.round(ProgressEvent.loaded / ProgressEvent.total * 100) + "%")
+      }
+    })
+    .then(res => {
+      // Update the mugshot image for the user
+      // set felon.src = result
+      console.log("Response: ", res)
+    })
+
+
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -11,17 +51,18 @@ function App() {
       </div>
       <div className="photos">
         <div className="photo-box">
-          <div className="image">
-          <img className="image" alt="Upload"></img>
+          <div >
+          <img className="image" src={image} alt="Upload"></img>
           </div>
-          <button>Upload Your Image</button>
+          <input type="file" onChange={fileSelectedHandler}/>
+          {/* <button onClick={fileUploadHandler}>Upload</button> */}
         </div>
         <div className="btn=box">
         <button className="btn">Find your Felon look-alike</button>
         </div>
         <div className="photo-box">
-          <div className="image">
-          <img  alt="Felon"></img>
+          <div >
+          <img className="image" src="https://www.mugshots.org/post/terry-nunez-mugshot" alt="Felon"></img>
           </div>
           <h3>Felon Info:</h3>
         </div>
