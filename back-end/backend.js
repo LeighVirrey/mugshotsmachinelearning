ff1f71a81c3836e34f8a20fac893ad4d2bc614a1
TOONZK <126704941+LeighVirrey@users.noreply.github.com>
2024-05-27 18:21:10 -0600

    euclidian math
    WHAT

--- back-end/backend.js
+++ back-end/backend.js
@@ -10,7 +10,7 @@ app.use(cors({origin: '*'}));
 app.use(express.static('./public'));
 const port = 9696;
 let net = null;
-let img = new Image();
+let img = null;
 
 app.get('/', async (req, res) => {
     res.send('hi');
@@ -46,8 +46,8 @@ app.get('/getImage', async (req, res) => {
         //img.onload = async () => {return};
         img.src = fs.readFileSync('public/images/userPhoto.jpg');
         let userFace = await net.estimateFaces(createCanvasFromImage());
-    
-        if (userFace[0] && userFace[0].faceInViewConfidence >= 0.9) {
+        if (userFace.length !== 0) {
+            //the asyncronousity of img.src sucks
             console.log('math start')
             let mostSimilarMugshot = await findMostSimilarMugshot(userFace);
             console.log('math end');
@@ -55,12 +55,13 @@ app.get('/getImage', async (req, res) => {
         }else{
             res.send('No face detected');
         }
-        
         fs.unlinkSync('public/images/userPhoto.jpg');
     }else{
         res.send('No image uploaded, upload an image by calling /upload first');
     }
 })
+
+//notes on math for presentation, for some reason Mugshot 43 was the one being hit the most, so I've opted to remove him and save him in my folders for later presenting
 
 function WackMath(face){
     //console.log(face)
@@ -83,29 +84,29 @@ function WackMath(face){
     //Don't even take this one seriously, I made it when I was tired and I didn't even use it because I came up with a better one right after
 }
 
-function calculateCentroid(annotation) {
-    let centroid = [0, 0, 0];
-    annotation.forEach((point) => {
-        centroid[0] += point[0];
-        centroid[1] += point[1];
-        centroid[2] += point[2];
-    })
-    centroid = centroid.map(val => val / annotation.length);
-    return centroid;
-}
+// function calculateCentroid(annotation) {
+//     let centroid = [0, 0, 0];
+//     annotation.forEach((point) => {
+//         centroid[0] += point[0];
+//         centroid[1] += point[1];
+//         centroid[2] += point[2];
+//     })
+//     centroid = centroid.map(val => val / annotation.length);
+//     return centroid;
+// }
 
-function attemptMath(face){
-    let keyNumbers = [];
-    Object.keys(face).forEach((key) => {
-        let keyArray = [];
-        face[key].forEach((point) => {
-            let numbersAdded = point[0] + point[1] + point[2];
-            keyArray.push(numbersAdded);
-        })
-        keyNumbers.push(keyArray.reduce((a, b) => a + b, 0) / keyArray.length);
-    })
-    return keyNumbers;
-}
+// function attemptMath(face){
+//     let keyNumbers = [];
+//     Object.keys(face).forEach((key) => {
+//         let keyArray = [];
+//         face[key].forEach((point) => {
+//             let numbersAdded = point[0] + point[1] + point[2];
+//             keyArray.push(numbersAdded);
+//         })
+//         keyNumbers.push(keyArray.reduce((a, b) => a + b, 0) / keyArray.length);
+//     })
+//     return keyNumbers;
+// }
 // function comparisonMath(face1, face2){
 //     let totalDifference = 0;
 //     Object.keys(face1).forEach((key) => {
@@ -119,35 +120,15 @@ function attemptMath(face){
 // }
 
 
-function comparisonMath(face1, face2){
-    let keyNumbers1 = attemptMath(face1);
-    let keyNumbers2 = attemptMath(face2);
-    let total1 = keyNumbers1.reduce((a, b) => a + b, 0);
-    let total2 = keyNumbers2.reduce((a, b) => a + b, 0);
-    return Math.abs(total1 - total2);
-    //AVERAGE MATH
-}
+// function comparisonMath(face1, face2){
+//     let keyNumbers1 = attemptMath(face1);
+//     let keyNumbers2 = attemptMath(face2);
+//     let total1 = keyNumbers1.reduce((a, b) => a + b, 0);
+//     let total2 = keyNumbers2.reduce((a, b) => a + b, 0);
+//     return Math.abs(total1 - total2);
+//     //AVERAGE MATH
+// }
 
-async function findMostSimilarMugshot(userFace) {
-    let minDifference = Infinity;
-    let mostSimilarMugshotIndex = -1;
-
-    for (let i = 1; i <= 60; i++) {
-        img.src = fs.readFileSync(`public/images/Mugshots/${i}.png`);
-        const mugFace = await net.estimateFaces(createCanvasFromImage());
-
-        if (mugFace[0] && mugFace[0].faceInViewConfidence >= 0.9) {
-            let difference = comparisonMath(userFace[0].annotations, mugFace[0].annotations);
-
-            if (difference < minDifference) {
-                minDifference = difference;
-                mostSimilarMugshotIndex = i;
-            }
-        }
-    }
-    //REGULAR MATH
-    return mostSimilarMugshotIndex !== -1 ? `http://localhost:9696/images/Mugshots/${mostSimilarMugshotIndex}.png` : null;
-}
 // async function findMostSimilarMugshot(userFace) {
 //     let minDifference = Infinity;
 //     let mostSimilarMugshotIndex = -1;
@@ -165,14 +146,70 @@ async function findMostSimilarMugshot(userFace) {
 //             }
 //         }
 //     }
-
+//     //average MATH
 //     return mostSimilarMugshotIndex !== -1 ? `http://localhost:9696/images/Mugshots/${mostSimilarMugshotIndex}.png` : null;
-//CENTROID MATH
 // }
 
+function calculateCentroid(annotation) {
+    let centroid = [0, 0, 0];
+    annotation.forEach((point) => {
+        centroid[0] += point[0];
+        centroid[1] += point[1];
+        centroid[2] += point[2];
+    })
+    centroid = centroid.map(val => val / annotation.length);
+    return centroid;
+    //Euclidian
+}
+
+function euclideanDistance(point1, point2) {
+    let sum = 0;
+    for (let i = 0; i < point1.length; i++) {
+        sum += Math.pow(point1[i] - point2[i], 2);
+    }
+    return Math.sqrt(sum);
+    //Euclidian
+}
+
+function comparisonMath(face1, face2){
+    let totalDifference = 0;
+    Object.keys(face1).forEach((key) => {
+        let centroid1 = calculateCentroid(face1[key]);
+        let centroid2 = calculateCentroid(face2[key]);
+        let difference = euclideanDistance(centroid1, centroid2);
+        totalDifference += difference;
+    });
+    return totalDifference;
+    //Euclidian I won't lie to you, the previous math kept return mugshot43 for multiple userPhotos so I asked copilot for help on this one.
+    //It seems the Euclidian math is the most accurate I can get it now, and it still returns mugshot43 for some reason, which means it's 43's problem and his face it just too dang old, probably from the beard.
+}
+
+
+async function findMostSimilarMugshot(userFace) {
+    let minDifference = Infinity;
+    let mostSimilarMugshotIndex = -1;
+
+    for (let i = 1; i <= 59; i++) {
+        img.src = fs.readFileSync(`public/images/Mugshots/${i}.png`);
+        const mugFace = await net.estimateFaces(createCanvasFromImage());
+
+        if (mugFace[0] && mugFace[0].faceInViewConfidence >= 0.9) {
+            let difference = comparisonMath(userFace[0].annotations, mugFace[0].annotations);
+
+            if (difference < minDifference) {
+                minDifference = difference;
+                mostSimilarMugshotIndex = i;
+            }
+        }
+    }
+
+    return mostSimilarMugshotIndex !== -1 ? `http://localhost:9696/images/Mugshots/${mostSimilarMugshotIndex}.png` : null;
+ //Euclidian
+}
+
 function createCanvasFromImage(){
-    const canvas =  createCanvas(img.width, img.height);
-    const ctx =  canvas.getContext('2d');
+    let canvas =  createCanvas(img.width, img.height);
+    let ctx =  canvas.getContext('2d');
      ctx.drawImage(img, 0, 0, img.width, img.height);
     return canvas;
 }
@@ -180,8 +217,10 @@ function createCanvasFromImage(){
 
 app.listen(port, async () => {
     console.log(`Server is running on port ${port}`);
+    console.log('Loading model, PLEASE WAIT FOR THE MODEL TO BE LOADED BEFORE DOING ANYTHING ELSE. If the model is not loading, restart the server');
     net = await facemesh.load();
     console.log('Model loaded');
+    img = new Image();
 
 
     //this is just for math testing, remove this later
@@ -198,6 +237,10 @@ app.listen(port, async () => {
     // }else{
     //     console.log(comparisonMath(mugFace[0].annotations, userFace[0].annotations));
     // }
-
+    // img.src = fs.readFileSync('public/images/Mugshots/43.png');
+    // let mugFace = await net.estimateFaces(createCanvasFromImage());
+    // img.src = fs.readFileSync('public/images/userPhoto.jpg');
+    // let userFace = await net.estimateFaces(createCanvasFromImage());
+    // console.log(comparisonMath(mugFace[0].annotations,userFace[0].annotations));
 })
 
